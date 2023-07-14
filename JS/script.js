836e50f38d8e5fbb4287013e95b2397ef275a905
SunStudio <90275380+SunTStudio@users.noreply.github.com>
2023-07-14 11:43:18 +0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,79 @@
+
+// Music Player Start
+function safeSound(){
+	var fileMusik = document.getElementById('song');
+	fileMusik.setAttribute("src", "audio/SafeSound.mp3");
+
+	var namaMusik = document.getElementById('nameSong');
+	namaMusik.innerHTML = "Safe And Sound";
+
+	var namaPenyanyi = document.getElementById('nameSinger');
+	namaPenyanyi.innerHTML = "Different";
+
+	// var iconMusik = document.getElementById('imgMusic');
+	// iconMusik.setAttribute("src","img/");
+}
+
+function rungkad(){
+	var fileMusik = document.getElementById('song');
+	fileMusik.setAttribute("src", "audio/Rungkad.mp3");
+
+	var namaMusik = document.getElementById('nameSong');
+	namaMusik.innerHTML = "Rungkad";
+
+	var namaPenyanyi = document.getElementById('nameSinger');
+	namaPenyanyi.innerHTML = "Happy Asmara";
+
+	// var iconMusik = document.getElementById('imgMusic');
+	// iconMusik.setAttribute("src","img/");
+}
+
+function ello(){
+	var fileMusik = document.getElementById('song');
+	fileMusik.setAttribute("src", "audio/PergiKembali.mp3");
+
+	var namaMusik = document.getElementById('nameSong');
+	namaMusik.innerHTML = "Pergi Untuk Kembali";
+
+	var namaPenyanyi = document.getElementById('nameSinger');
+	namaPenyanyi.innerHTML = "Ello";
+
+	// var iconMusik = document.getElementById('imgMusic');
+	// iconMusik.setAttribute("src","img/");
+}
+
+function mwam(){
+	var fileMusik = document.getElementById('song');
+	fileMusik.setAttribute("src", "audio/Raise Your Flag.mp3");
+
+	var namaMusik = document.getElementById('nameSong');
+	namaMusik.innerHTML = "Raise Your Flag";
+
+	var namaPenyanyi = document.getElementById('nameSinger');
+	namaPenyanyi.innerHTML = "Man With A Mission";
+
+	// var iconMusik = document.getElementById('imgMusic');
+	// iconMusik.setAttribute("src","img/");
+}
+
+function myWay(){
+	var fileMusik = document.getElementById('song');
+	fileMusik.setAttribute("src", "audio/myWay.mp3");
+
+	var namaMusik = document.getElementById('nameSong');
+	namaMusik.innerHTML = "My Way";
+
+	var namaPenyanyi = document.getElementById('nameSinger');
+	namaPenyanyi.innerHTML = "Frank Sinastra";
+
+	// var iconMusik = document.getElementById('imgMusic');
+	// iconMusik.setAttribute("src","img/");
+}
+
+// Music Player End
+
+//  Langganan Start
+
 function subsFree() {
 	var paket = document.getElementById('paketHari');
 	paket.innerHTML = "Paket Free";
@@ -49,3 +125,5 @@ function subsYears(){
 	var kadaluarsa = document.getElementById('kadaluarsa');
 	kadaluarsa.innerHTML = "*Masa Berlaku Langganan Satu Tahun";
 }
+
+// Langganan End
